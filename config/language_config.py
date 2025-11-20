cf59c2c0c4eda7e3dea62d0322f85d6444897bfb
Kiran Mondal <kiranmondal5516@gmail.com>
2025-11-20 21:12:35 +0530

    Create language_config.py

---
+++ config/language_config.py
@@ -0,0 +1,23 @@
+LANGUAGES = {
+    "EN": {
+        "enter_password": "Enter password:",
+        "weak": "Weak password!",
+        "strong": "Strong password!",
+        "leaked": "⚠️ Password found in leak!",
+        "safe": "✔️ Password is safe."
+    },
+    "BN": {
+        "enter_password": "পাসওয়ার্ড লিখুন:",
+        "weak": "দুর্বল পাসওয়ার্ড!",
+        "strong": "শক্তিশালী পাসওয়ার্ড!",
+        "leaked": "⚠️ পাসওয়ার্ড ফাঁস হয়েছে!",
+        "safe": "✔️ পাসওয়ার্ড নিরাপদ।"
+    },
+    "HI": {
+        "enter_password": "पासवर्ड दर्ज करें:",
+        "weak": "कमज़ोर पासवर्ड!",
+        "strong": "मज़बूत पासवर्ड!",
+        "leaked": "⚠️ पासवर्ड लीक मिला!",
+        "safe": "✔️ पासवर्ड सुरक्षित है।"
+    }
+}
