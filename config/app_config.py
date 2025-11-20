33d52a80e47cde68feff4193ae2a16e453fed937
Kiran Mondal <kiranmondal5516@gmail.com>
2025-11-20 21:11:33 +0530

    Create app_config.py

---
+++ config/app_config.py
@@ -0,0 +1,13 @@
+APP_NAME = "Password Guard"
+APP_VERSION = "1.0.0"
+
+LOGO_PATH = "assets/logo.svg"
+ICON_PATH = "assets/icon.svg"
+
+DEFAULT_LANGUAGE = "EN"
+
+ENABLE_CLOUD_SYNC = True
+ENABLE_AI_STRENGTH = True
+ENABLE_ALERT_SYSTEM = True
+
+THEME_COLOR = "#0D4FF0"  # blue
